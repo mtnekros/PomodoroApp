b8ad34c162a609adcf2d8df9215cb9635b1df158
Diwash Tamang <mtnekros@gmail.com>
2020-06-02 10:18:32 +0545

    refactor: set MessageModal as child widget of TimerFrame

--- message_modal.py
+++ message_modal.py
@@ -6,62 +6,54 @@ import time
 class MessageModal(Toplevel):
     is_already_open = False
 
-    def __init__(self, timer):
-        super().__init__( bg="white")
-        self.set_up_modal_attributes()
+    def __init__(self, root, time_label_var, modal_should_close):
+        super().__init__(root, bg="white")
         if MessageModal.is_already_open:
             self.destroy()
             return
         MessageModal.is_already_open = True
         self.__is_open = True
-        self.timer = timer
-        self.timer_mode = timer.get_mode()
-        self.create_widgets()
-        # add event listeners for closing
-        self.bind("<Escape>", lambda event: self.close())
-        self.bind("<space>", lambda event: self.close())
-        self.update()
+        self.modal_should_close = modal_should_close
+        self.set_up_modal_attributes()
+        self.create_widgets(time_label_var)
+        self.bind_hide_events()
+        self.hide()
 
     def set_up_modal_attributes(self):
         # settup the semitransparent background
         self.wm_attributes("-alpha", 0.3)
         self.wm_attributes("-fullscreen", True)
-        self.attributes("-topmost", True)
-        self.attributes("-topmost", False)
         self.wm_overrideredirect(True) # hide from taskbar
-        self.focus_set()
         # setting up the top layer in front of the background
         self.container = Toplevel(self, bg='magenta')
         self.container.attributes("-fullscreen", True)
         self.container.wm_attributes("-alpha", 0.85)
         self.container.wm_attributes("-transparentcolor", 'magenta')
-        self.container.attributes("-topmost", True)
         self.container.overrideredirect(True) # hide from taskbar
 
-    def create_widgets(self):
+    def create_widgets(self, time_label_var):
         self.frame = Frame(self.container,  bg="#111")
         self.frame.place(relx=.5, rely=.5, anchor="center", width=440, height=210)
-        self.time_label_var = StringVar(self.frame)
-        self.time_label = Label(self.frame, textvariable=self.time_label_var, bg="#111", foreground="white", font=("Cookie", 75))
+        self.time_label = Label(self.frame, textvariable=time_label_var, bg="#111", foreground="white", font=("Cookie", 75))
         self.time_label.place(relx=.5, rely=.43, anchor="center")
         caption = Label(self.frame, text="Time to take a break.", bg="#111", foreground="white", font=("Century Gothic", 13))
         caption.place(relx=.5, rely=.7, anchor="center")
-    
-    def update(self):
-        if not self.timer.is_running():
-            self.close()
-        if self.__is_open:
-            time_left = self.timer.get_time_left()
-            self.time_label_var.set( Timer.format_time( time_left ) )
-            if self.timer.get_mode() != self.timer_mode:
-                self.close()
-            else:
-                self.after(10, self.update)
 
-    def close(self):
-        if self.__is_open:
-            self.__is_open = False
-            self.unbind("<Escape>")
-            self.unbind("<Motion>")
-            MessageModal.is_already_open = False
-            self.destroy()
+    def bind_hide_events(self):
+        hide = lambda _ : self.hide()
+        self.bind("<Escape>", hide)
+        self.bind("<space>", hide)
+        self.container.bind("<Escape>", hide)
+        self.container.bind("<space>", hide)
+    
+    def show(self):
+        self.focus_set()
+        self.deiconify()
+        self.container.deiconify()
+        self.attributes("-topmost", True)
+        self.attributes("-topmost", False)
+        self.container.attributes("-topmost", True)
+
+    def hide(self):
+        self.withdraw()
+        self.container.withdraw()
